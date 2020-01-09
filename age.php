ead6da293888e154fe6e07d14b433e67b265ff2a
Marcelus Sophie Laube <51539053+SophieLaube@users.noreply.github.com>
2020-01-09 11:45:16 -0500

    Add files via upload
    c'est un fichier php qui determine si une personne est enfant, mineur, majeur, ou âgé à partir de l'âge saisie.

---
+++ age.php
@@ -0,0 +1,21 @@
+<?php
+
+$valeur = 12;
+
+if($valeur >=3 && $valeur <=12){
+    echo "C'est un enfant";
+}
+
+if($valeur >=13 && $valeur <= 17){
+    echo "C'est un mineur";
+}
+
+if($valeur >=18 && $valeur <= 54){
+    echo "C'est un majeure";
+}
+
+if($valeur >=55){
+    echo " Age";
+}
+
+?>
